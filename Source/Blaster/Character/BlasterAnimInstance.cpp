362872505f654b395846f94a75bab23320adbea3
piyush tadvi <piyushtadvi4@gmail.com>
2023-11-16 15:30:03 +0000

    Implemented Character movement and animation blueprint

---
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterAnimInstance.h"
+#include "BlasterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UBlasterAnimInstance::NativeInitializeAnimation()
+{
+	//initialize character class 
+	BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+}
+
+void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	if (BlasterCharacter == nullptr)
+	{
+		BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+	}
+
+	if (BlasterCharacter == nullptr) return;
+
+	//getting character speed to implement perform character animation..
+	FVector Velocity = BlasterCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	//checking if character is in air or not..
+	bIsInAir = BlasterCharacter->GetCharacterMovement()->IsFalling();
+
+	//checking if any movement input getting by player..
+	bIsAccelerating = BlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+}
---
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ABlasterCharacter::ABlasterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetMesh());
+	SpringArm->TargetArmLength = 600.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+
+}
+
+// Called when the game starts or when spawned
+void ABlasterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABlasterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABlasterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &ABlasterCharacter::Turn);
+	PlayerInputComponent->BindAxis("Lookup", this, &ABlasterCharacter::Lookup);
+
+}
+
+void ABlasterCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation = FRotator(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction * Value);
+	}
+}
+
+void ABlasterCharacter::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation = FRotator(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction * Value);
+	}
+}
+
+void ABlasterCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ABlasterCharacter::Lookup(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
